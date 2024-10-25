48b13474aaadb4303c5f3997a59feecfdd51b5e4
aimelec <gabrielleclercq@amalgama.co>
2024-10-25 20:56:32 -0300

    Adding node API example

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from 'hono'
+// @ts-ignore
+import path from 'node:path'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono (which is flame in japanese)!')
+})
+
+app.get('/path', (c) => {
+  return c.text(path.join('/foo', 'bar', 'baz/asdf'));
 })
 
 export default app
