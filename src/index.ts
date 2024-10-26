df335b9d1abc0cee18b2f47e2274ccb44d3315fe
aimelec <gabrielleclercq@amalgama.co>
2024-10-25 21:13:04 -0300

    Adding database config

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,17 @@ app.get('/path', (c) => {
   return c.text(path.join('/foo', 'bar', 'baz/asdf'));
 })
 
+app.get("/query", async (c) => {
+  try {
+    let { results } = await c.env.DB.prepare(
+      "SELECT * FROM Customers",
+    )
+      .bind()
+      .all();
+    return c.json(results);
+  } catch (e) {
+    return c.json({ err: (e as Error).message }, 500);
+  }
+});
+
 export default app
